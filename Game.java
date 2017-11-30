ae9da5f08b3446d2dbd8cbdc4226e031b8f84b8e
JofipeRo <34068128+JofipeRo@users.noreply.github.com>
2017-11-30 10:57:27 +0000

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,6 +7,7 @@ public class Game {
 	private int counterReinos;
 	private int xMap;
 	private int yMap;
+	private int currentPlayer;
 	public Reinos[] r1;
 	public Castelos[] c1;
 	public boolean isGameOn() {
@@ -22,31 +23,68 @@ public class Game {
 		counterCastles = 0;
 		counterReinos = 0;
 	}
+	public int getNReino() {
+		return numberReinos;
+	}
+	public int getNCastles() {
+		return numberCastles;
+	}
 	public void addCastles(Castelos c2, String name, int treasure, int x, int y) {
 		c2 = new Castelos(name, treasure, x, y);
 		c1[counterCastles++]=c2;
 	}
+	public int getCounterCastles() {
+		return counterCastles;
+	}
+	public void removeCastles() {
+		counterCastles--;
+	}
 	public boolean hasNextCastles() {
 		return counterCastles < numberCastles;
 	}
-	// Fazer metodo para econtrar o nome do castelo, e devolver a posicao, para depois definir o owner e tesouro
 	public void addReinos (Reinos r2, String name, String castle, int treasure) {
+		if(choseCastle(castle, c1)!=-1) {
+			treasure=c1[choseCastle(castle,c1)].getTreasure();
+		}
 		r2 = new Reinos(name, treasure);
 		r1[counterReinos++] = r2;
-		
+		c1[choseCastle(castle,c1)].changeOwner(r1[counterReinos-1]);
 	}
 	public boolean hasNextReinos() {
 		return counterCastles < numberCastles;
 	}
-	public String getTeamName(int i) {
-		return r1[i].getReinoName();
+	public String getTeamName() {
+		return r1[currentPlayer].getReinoName();
 	}
 	
+	public int choseCastle(String name, Castelos[] c1) {
+		int i=-1;
+		int j = 0;
+		boolean found = false;
+		while(!found && j<counterCastles) {
+			if(name.equals(c1[j].getName())){
+				i=j;
+				found=true;
+			}
+			else {
+				j++;
+			}
+		}
+		return i;
+	}
 	
-	
-	
-	public boolean hasNextPlayer(int i) {
-		return i < numberReinos;
+	public void inicializeInteratiorPlayer() {
+		currentPlayer=0;
+	}
+	public boolean hasNextPlayer() {
+		return currentPlayer+1 < numberReinos;
+	}
+	public void nextPlayer() {
+		if(hasNextPlayer())
+			currentPlayer++;
+		else {
+			currentPlayer=0;
+		}
 	}
 	
 	
@@ -58,5 +96,11 @@ public class Game {
 	}
 	public int getYMap() {
 		return yMap;
+	}
+	public void startGame() {
+		gameOn=true;
+	}
+	public void closeGame() {
+		gameOn=false;
 	}
 }
