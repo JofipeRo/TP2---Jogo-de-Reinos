07a9f759a84b33f2c9644dc8f2c1d1220460fc09
JofipeRo <34068128+JofipeRo@users.noreply.github.com>
2017-11-30 10:59:52 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,18 +18,17 @@ public class Main {
 		String command = readCommand(s1);
 		printMenu();
 		Game g1 = null;
-		int i = 0;
+		Condicoes cond= new Condicoes();
+		g1.inicializeInteratiorPlayer();
 		while (!command.equals(SAI)) {
-			if (!g1.isGameOn()) {
+			if (!g1.isGameOn() || g1==null) {
 				System.out.print("> ");
 			} else {
-				System.out.print(g1.getTeamName(i) + " > ");
-				i++;
-				if(!g1.hasNextPlayer(i))
-					i=0;
+				System.out.print(g1.getTeamName() + " > ");
+				g1.nextPlayer();
 				}
 			command = readCommand(s1);
-			executeCommand(command,s1);
+			executeCommand(command,s1, g1, cond);
 		}
 		s1.close();
 		System.out.println("Obrigado por jogar. Ate a proxima.");
@@ -44,12 +43,60 @@ public class Main {
 		System.out.println("ajuda - Mostra a ajuda");
 		System.out.println("sai - Termina a execucao do programa");
 	}
-	private static void executeCommand(String command, Scanner s1) {
+	private static void executeCommand(String command, Scanner s1, Game g1, Condicoes cond) {
 		switch(command) {
 		case AJUDA:
 			System.out.println("novo - Novo jogo");
 			System.out.println("ajuda - Mostra a ajuda");
 			System.out.println("sai - Termina a execucao do programa");
+		case NOVO_JOGO:
+			g1.closeGame();
+			int x = s1.nextInt();
+			int y = s1.nextInt();
+			int nReinos = s1.nextInt();
+			int nCastles = s1.nextInt();
+			s1.nextLine();
+			Castelos c2=null;
+			Reinos r2=null;
+			String CastleName;
+			int castleTreasure;
+			int xC;
+			int yC;
+			String reinoName;
+			String castle;
+			int totalTreasure=0;
+			Castelos[] c1=null;
+			Reinos[] r1=null;
+			 g1 = new Game(x, y, nReinos, nCastles, c1, r1);
+			 if(!cond.mapSize(g1))
+				 System.out.println("Mapa pequeno demais para o jogo.");
+			 if(cond.mapSize(g1) && !cond.numberReinos(g1))
+				 System.out.println("Numero de reinos invalido.");
+			 if(cond.mapSize(g1) && cond.numberReinos(g1) && !cond.numberCastles(g1))
+				 System.out.println("Numero de castelos invalido.");
+			 if(cond.mapSize(g1) && cond.numberReinos(g1) && cond.numberCastles(g1)) {
+			 while(g1.hasNextCastles()) {
+				xC = s1.nextInt();
+				yC = s1.nextInt();
+				castleTreasure = s1.nextInt();
+				CastleName=s1.nextLine();
+				g1.addCastles(c2, CastleName, castleTreasure, xC, yC);
+				if(!cond.goodCastlesCordinates(g1, c1)) {
+					System.out.println("Castelo em posicao invalida.");
+					g1.removeCastles();
+				}
+				if(cond.goodCastlesCordinates(g1, c1) && !cond.goodCastlesTreasure(g1, c1)) {
+					System.out.println("Castelo com riqueza invalida.");
+					g1.removeCastles();
+				}
+			 }
+			 while(g1.hasNextReinos()) {
+				 reinoName=s1.next();
+				 castle=s1.nextLine();
+				 g1.addReinos(r2, reinoName, castle, totalTreasure);
+			 }
+			 }
+			 g1.startGame();
 		}
 	}
 	
