285c4b2eedd805bfd35d719488172dbd3b16f279
JofipeRo <34068128+JofipeRo@users.noreply.github.com>
2017-12-14 23:24:28 +0000

    Add files via upload

--- Reino.java
+++ Reino.java
@@ -1,56 +1,142 @@
-
+/**
+ * @author joaor 53349 & Frederico 52794 
+ * Classe para definir o objeto Reino da ser utilizado pelo jogo.
+ */
 public class Reino {
+	// Variaveis
+	/**
+	 * teamName - nome do reino.
+	 * reinoSoldados - colecao de soldados do reino.
+	 * reinoCastelos - reino de castelos do reino.
+	 */
 	private String teamName;
 	private SoldadoColection reinoSoldados;
 	private CasteloColection reinoCastelos;
-	
+	// Metodos
+	/**
+	 * Metodo para criar um reino, com nome name e com tanho de colecao de castelos size.
+	 * @param name - nome do reino.
+	 * @param size - tamanho da colecao.
+	 * @pre - name nao e igual ao nome de outro reino.
+	 */
 	public Reino(String name, int size) {
 		reinoCastelos=new CasteloColection(size);
 		reinoSoldados=new SoldadoColection();
 		teamName=name;
 	}
+	/**
+	 * Metodo para devolver o nome do reino.
+	 * @return - Devolve o nome do reino.
+	 */
 	public String getReinoName() {
 		return teamName;
 	}
+	/**
+	 * Metodo para devolver o numero de castelos na colecao de soldados do reino.
+	 * @return - devolve o numero de castelos do reino.
+	 */
 	public int getNCastles() {
 		return reinoCastelos.getCounter();
 	}
+	/**
+	 * Metodo para devolver o numero de soldados na colecao de soldados do reino.
+	 * @return - devolve o numero de soldados do reino.
+	 */
 	public int getNSoldiers() {
 		return reinoSoldados.getCounterSoldados();
 	}
+	/**
+	 * Metodo para saber se existe um castelo com nome name, na colecao de castelos do reino.
+	 * @param name - nome do castelo a procurar.
+	 * @return - Devolve true se houver um castelo com nome name na colecao de castelos do reino.
+	 */
 	public boolean searchForCastle(String name) {
 		return reinoCastelos.nameFound(name);
 	}
+	/**
+	 * Metodo para adicionar um castelo a colecao de castelos do reino.
+	 * @param c2 - castelo a adicionar ao reino.
+	 */
 	public void addCastelo(Castelo c2) {
 		reinoCastelos.addCastelo(c2);
 	}
+	/**
+	 * Metodo para devolver o nome do castelo na posicao i da colecao de castelos.
+	 * @param i - posicao do castelo na colecao de castelos.
+	 * @return - devolve o nome do castelo.
+	 */
 	public String getCastleName(int i) {
 		return reinoCastelos.getName(i);
 	}
+	/**
+	 * Metodo para devolver o tesouro do castelo na posicao i da colecao de castelos.
+	 * @param i - posicao do castelo na colecao de castelos.
+	 * @return - devolve o tesouro do castelo.
+	 */
 	public int getCastleTreasure(int i) {
 		return reinoCastelos.getTreasure(i);
 	}
+	/**
+	 * Metodo para devolver a coordenada x do castelo na posicao i da colecao de castelos.
+	 * @param i - posicao do castelo na colecao de castelos.
+	 * @return - devolve a coordenada x do castelo.
+	 */
 	public int getCastleXPos(int i) {
 		return reinoCastelos.getXPos(i);
 	}
+	/**
+	 * Metodo para devolver a coordenada y do castelo na posicao i da colecao de castelos.
+	 * @param i - posicao do castelo na colecao de castelos.
+	 * @return - devolve a coordenada y do castelo.
+	 */
 	public int getCastleYPos(int i) {
 		return reinoCastelos.getYPos(i);
 	}
+	/**
+	 * Metodo para devolver o tesouro total do reino, ou seja, a soma do tesouro de todos os castelos do reino.
+	 * @return - devolve o tesouro total do reino.
+	 */
 	public int getTotalTreasure() {
 		return reinoCastelos.getSumTreasure();
 	}
+	/**
+	 * Metodo para devolver a coordenada x do soldado na posicao i da colecao de soldados.
+	 * @param i - posicao do soldado na colecao de soldados.
+	 * @return - devolve a coordenada x do soldado.
+	 */
 	public int getSoldadoXPos(int i) {
 		return reinoSoldados.getSoldadoXPos(i);
 	}
+	/**
+	 * Metodo para devolver a coordenada y do soldado na posicao i da colecao de soldados.
+	 * @param i - posicao do soldado na colecao de soldados.
+	 * @return - devolve a coordenada y do soldado.
+	 */
 	public int getSoldadoYPos(int i) {
 		return reinoSoldados.getSoldadoYPos(i);
 	}
+	/**
+	 * Metodo para devolver o tipo do soldado na posicao i da colecao de soldados.
+	 * @param i - posicao do soldado na colecao de soldados.
+	 * @return - devolve o tipo do soldado.
+	 */
 	public String getSoldadoType(int i) {
 		return reinoSoldados.getSoldadoType(i);
 	}
+	/**
+	 * Metodo para comparar nomes de reinos distintos, e determinar qual vem primeiro em ordem alfabetica.
+	 * @param other - outro objeto Reino com o qual iremos comparar o nome
+	 * @return - devolve true se o nome deste reino vier primeiro alfabeticamente que o nome do outro reino.
+	 */
 	public boolean greaterThan(Reino other) {
 		return this.getReinoName().compareTo(other.getReinoName())>0;
 	}
+	/**
+	 * Metodo para adicionar um soldado a colecao de soldados do reino de tipo type,
+	 * num certo castelo da colecao de castelos do reino.
+	 * @param type - tipo do soldado a adicionar.
+	 * @param castle - nome do castelos no qual o soldado vai ser criado (coordenadas equivalentes).
+	 */
 	public void addSoldadoToReino(String type, String castle) {
 		int i = getCastleWithName(castle);
 		int x;
@@ -61,12 +147,29 @@ public class Reino {
 		reinoCastelos.spendMoneyOfCastle(type, i);
 		
 	}
+	/**
+	 * Metodo para encontrar a posicao do castelo com nome castle na colecao de castelos do reino.
+	 * @param castle - nome do castelo a encontrar a posicao.
+	 * @return - posicao do castelo na colecao.
+	 */
 	public int getCastleWithName(String castle) {
 		return reinoCastelos.getCastleWithName(castle);
 	}
+	/**
+	 * Metodo para saber se o castelo com nome castle da colecao,
+	 * tem moedas suficientes para efetuar um recrutamento de custo money.
+	 * @param castle - nome do castelo de que queremos verificar a condicao.
+	 * @param money - custo de moedas do recrutamento.
+	 * @return - devolve true se o castelo tiver dinheiro suficiente.
+	 */
 	public boolean castleHasEnoughMoney(String castle, int money) {
 		return reinoCastelos.castleHasEnoughMoney(getCastleWithName(castle), money);
 	}
+	/**
+	 * Metodo para saber se um castelo de nome castle do reino esta ocupado por um soldado do reino.
+	 * @param castle - nome do castelo do reino.
+	 * @return - devolve true se o castelo estiver ocupado
+	 */
 	public boolean reinoCastleOcupado(String castle) {
 		boolean found=false;
 		for(int i = 0; i<getNSoldiers();i++) {
@@ -78,27 +181,69 @@ public class Reino {
 		}
 		return found;
 	}
+	/**
+	 * Devolve a String com as informacoes do soldado na posicao i da colecao de soldados.
+	 * @param i - posicao do soldado na colecao de soldados.
+	 * @return - devolve a informacao do soldado.
+	 */
 	public String printSoldadoInfo(int i) {
 		return reinoSoldados.printSoldadoInfo(i);
 	}
+	/**
+	 * Metodo para econtrar a posicao do soldado na colecao de soldados atraves das coordenadas xPos yPos.
+	 * @param xPos - coordenada x do soldado a encontrar a posicao.
+	 * @param yPos - coordenada y do soldado a encontrar a posicao.
+	 * @return - devolve a posicao do soldado na colecao de soldados.
+	 */
 	public int findSoldadoIndex(int xPos, int yPos) {
 		return reinoSoldados.getSoldadoIndex(xPos, yPos);
 	}
+	/**
+	 * Metodo para mover o soldado de posicao i na colecao de soldados para a certa direcao.
+	 * @param i - posicao do soldado a mover.
+	 * @param direcao - direcao para o qual o soldado deve mover-se.
+	 */
 	public void moveSoldado(int i, String direcao) {
 		reinoSoldados.move(i , direcao);
 	}
+	/**
+	 * Metodo para verificar se o soldado de posicao xPos, yPos colide com um outro soldado da mesma colecao.
+	 * @param xPos - coordenada x do soldado.
+	 * @param yPos - coordenada y do soldado.
+	 * @param soldado - posicao do soldado na colecao.
+	 * @param direcao - direcao para o qual so soldado pretende mover-se.
+	 * @return devolve true se houver colisao entre soldados.
+	 */
 	public boolean soldadoColision(int xPos,int yPos, int soldado, String direcao) {
 		return reinoSoldados.colision(xPos, yPos, soldado, direcao);
 	}
+	/**
+	 * Metodo para verificar se o soldado de posicao xPos yPos colide com um soldado deste reino.
+	 * @param xPos - coordenada x do soldado.
+	 * @param yPos - coordenada y do soldado.
+	 * @return - devolve tru se houver colisao entre soldados.
+	 */
 	public int enemyColision(int xPos, int yPos) {
 		return reinoSoldados.enemyColision(xPos, yPos);
 	}
+	/**
+	 * Metodo para matar um soldado deste reino e remove-lo da colecao de soldados.
+	 * @param soldado - posicao do soldado na colecao de soldados
+	 */
 	public void killSoldadoFromReino(int soldado) {
 		reinoSoldados.killSoldado(soldado);
 	}
+	/**
+	 * Metodo para remover um castelo da colecao de castelos deste reino.
+	 * @param name - mome do castelo a remover da colecao.
+	 */
 	public void removeCastleFromReino(String name) {
 		reinoCastelos.removeCastle(name);
 	}
+	/**
+	 * Metodo para saber se o reino ainda sobrevive, ou seja, tem pelo menos ou 1 castelo ou 1 soldado.
+	 * @return - devolve true se o reino ainda sobrevive.
+	 */
 	public boolean isReinoAlive() {
 		return reinoCastelos.getCounter()>0 || reinoSoldados.getCounterSoldados()>0;
 	}
--- ReinoColection.java
+++ ReinoColection.java
@@ -1,31 +1,78 @@
-
+/**
+ * @author joaor 53349 & Frederico 52794 
+ * Classe para definir uma colecao de reinos de um jogo.
+ */
 public class ReinoColection {
+	// Variaveis.
+	/**
+	 * r1 - vetor de reinos.
+	 * counterReino - valor do counter do vetor reino.
+	 */
 	private Reino[] r1;
 	private int counterReino;
-	
+	// Metodos
+	/**
+	 * Metodo para criar uma colecao de reinos de tamanho size e com counter inicializado a 0.
+	 * @param size - tamanho do vetor.
+	 */
 	public ReinoColection(int size) {
 		r1=new Reino[size];
 		counterReino=0;
 	}
+	/**
+	 * Metodo para adicionar um reino a colecao com um castelo para que cumpra os requesitos de castelo vivo.
+	 * @param r2 - reino a adicionar a coleacao.
+	 * @param c2 - castelo a adicionar ao reino.
+	 */
 	public void addReino(Reino r2, Castelo c2) {
 		r1[counterReino]=r2;
 		r1[counterReino++].addCastelo(c2);
 	}
+	/**
+	 * Metodo para devolver o nome do reino na posicao i da colecao
+	 * @param i - posicao do reino na colecao.
+	 * @return - devolve o nome do reino.
+	 */
 	public String getName(int i) {
 		return r1[i].getReinoName();
 	}
+	/**
+	 * Metodo para devolver o numero de castelos do reino na posicao i da colecao
+	 * @param i - posicao do reino na colecao.
+	 * @return - devolve o numero de castelos do reino.
+	 */
 	public int numberCastelos(int i) {
 		return r1[i].getNCastles();
 	}
+	/**
+	 * Metodo para devolver numero de soldados do reino na posicao i da colecao
+	 * @param i - posicao do reino na colecao.
+	 * @return - devolve o numero de soldados do reino.
+	 */
 	public int numberSoldiers(int i) {
 		return r1[i].getNSoldiers();
 	}
+	/**
+	 * Metodo para devolver o tesouro total do reino na posicao i da colecao
+	 * @param i - posicao do reino na colecao.
+	 * @return - devolve o tesouro total do reino.
+	 */
 	public int getTotalTreasure(int i) {
 		return r1[i].getTotalTreasure();
 	}
+	/**
+	 * Metodo para devolver o valor do counter da colecao.
+	 * @return - devolve o valor de counterReino.
+	 */
 	public int getCounter() {
 		return counterReino;
 	}
+	/**
+	 * Metodo para encontrar se algum reino da colecao possui o castelo de nome name.
+	 * @param name - nome do castelo a procurar.
+	 * @return - Devolve -1 se nenhum reino da colecao possuir o castelo, caso contrario
+	 * devolve a posicao do reino que possui o castelo.
+	 */
 	public int searchForCastle(String name) {
 		int i=-1;
 		for(int i2=0; i2<counterReino; i2++) {
@@ -34,6 +81,11 @@ public class ReinoColection {
 		}
 		return i;
 	}
+	/**
+	 * Metodo para saber se certo name ja foi utilizado para nome de algum reino da colecao.
+	 * @param name - nome a pesquisar.
+	 * @return - devolve true se ja houver um reino com nome name.
+	 */
 	public boolean nameFound(String name) {
 		boolean found=false;
 		for(int i=0;i<counterReino;i++) {
@@ -42,19 +94,49 @@ public class ReinoColection {
 		}
 		return found;
 	}
+	/**
+	 * Metodo para adicionar um castelo ao reino de posicao i na colecao.
+	 * @param c2 - castelo a adicionar.
+	 * @param i - posicao do reino ao qual vamos adicionar o castelo.
+	 */
 	public void addCasteloToReino(Castelo c2, int i) {
 		r1[i].addCastelo(c2);
 	}
+	/**
+	 * Metodo para devolver o nome de um castelo de posicao i na colecao de reinos do reino de posicao player nesta colecao.
+	 * @param i - posicao do castelo na colecao de castelos do reino.
+	 * @param player - posicao do reino desta colecao.
+	 * @return - devolve o nome do castelo.
+	 */
 	public String getReinoCastelosName(int i, int player) {
 		return r1[player].getCastleName(i);
 	}
+	/**
+	 * Metodo para devolver o tesouro de um castelo de posicao i na colecao de reinos do reino de posicao player nesta colecao.
+	 * @param i - posicao do castelo na colecao de castelos do reino.
+	 * @param player - posicao do reino desta colecao.
+	 * @return - devolve o tesouro do castelo.
+	 */
 	public int getReinoCastelosTreasure(int i, int player) {
 		return r1[player].getCastleTreasure(i);
 	}
+	/**
+	 * Metodo para devolver as coordenadas x y de um castelo,
+	 * de posicao i na colecao de reinos do reino de posicao player nesta colecao, na forma "x,y".
+	 * @param i - posicao do castelo na colecao de castelos do reino.
+	 * @param player - posicao do reino desta colecao.
+	 * @return - devolve as coordenadas do castelo.
+	 */
 	public String getReinoCastelosPos(int i, int player) {
 		String Pos=r1[player].getCastleXPos(i)+","+r1[player].getCastleYPos(i);
 		return Pos;
 	}
+	/**
+	 * Metodo para devolver a informacao do reino de posicao k de um vetor auxiliar no qual os reinos estao listados por ordem
+	 * alfabetica, na forma de nomeReino, numero de castelos, numero de soldados, riqueza total.
+	 * @param k - posicao do reino no vetor auxiliar.
+	 * @return - devolve a informacao do reino.
+	 */
 	public String getReinoInfo(int k) {
 		Reino[] rr = new Reino[counterReino];
 		Reino r2;
@@ -77,40 +159,128 @@ public class ReinoColection {
 		return reinoInfo;
 		}
 	
-	
+	/**
+	 * Metodo para adicionar um soldado de tipo type ao reino de posicao i nesta colecao, num castelo castle desse mesmo reino.
+	 * @param type - tipo do soldado.
+	 * @param castle - castelo no qual o soldado deve ser criado.
+	 * @param i - posicao do reino nesta colecao.
+	 */
 	public void addSoldadosToReino(String type, String castle, int i) {
 		r1[i].addSoldadoToReino(type, castle);
 	}
+	/**
+	 * Metodo para saber se um reino de posicao i nesta colecao, tem o castelo de nome castle.
+	 * @param i - posicao do reino nesta colecao.
+	 * @param castle - nome do castelo a averiguar.
+	 * @return - devolve true se o reino tiver o castelo.
+	 */
 	public boolean reinoHasCastle(int i, String castle) {
 		return r1[i].getCastleWithName(castle)!=-1;
 	}
+	/**
+	 * Metodo para saber se o castelo de nome castle do reino de posicao i nesta colecao
+	 * tem moedas suficientes para efetuar um recrutameto de custo money.
+	 * @param i - posicao do reino desta colecao.
+	 * @param castle - nome do castelo a verificar.
+	 * @param money - custo de moedas a verificar.
+	 * @return devolve true se o castelo tiver moedas suficientes.
+	 */
 	public boolean castleHasEnoughMoney(int i, String castle, int money) {
 		return r1[i].castleHasEnoughMoney(castle, money);
 	}
+	/**
+	 * Metodo para saber se o castelo de nome caste do reino de posicao i nesta colecao
+	 * esta ocupado por algum soldado desse mesmo reino.
+	 * @param i - posicao do reino nesta colecao.
+	 * @param castle - nome do castelo nesta colecao.
+	 * @return - devolve true se o castelo estiver ocupado.
+	 */
 	public boolean isReinoCastleOcupado(int i, String castle) {
 		return r1[i].reinoCastleOcupado(castle);
 	}
+	/**
+	 * Metodo para devolver a informacao de um soldado de posicao i na colecao de soldados
+	 * do reino de posicao player desta colecao.
+	 * @param i - posicao do soldado na colecao de soldados do reino.
+	 * @param player - posicao do reino nesta colecao.
+	 * @return - devolve a informacao do soldado.
+	 */
 	public String printReinoSoldadoInfo(int i, int player) {
 		return r1[player].printSoldadoInfo(i);
 	}
+	/**
+	 * Metodo para devolver a coordenada x do soldado de posicao i na colecao de soldados
+	 * do reino de posicao player desta colecao.
+	 * @param i - posicao do soldado na colecao de soldados do reino.
+	 * @param player - posicao do reino nesta colecao.
+	 * @return - devolve a coordenada x do soldado.
+	 */
 	public int getReinoSoldadoXPos(int i, int player) {
 		return r1[player].getSoldadoXPos(i);
 	}
+	/**
+	 * Metodo para devolver a coordenada y do soldado de posicao i na colecao de soldados
+	 * do reino de posicao player desta colecao.
+	 * @param i - posicao do soldado na colecao de soldados do reino.
+	 * @param player - posicao do reino nesta colecao.
+	 * @return - devolve a coordenada y do soldado.
+	 */
 	public int getReinoSoldadoYPos(int i, int player) {
 		return r1[player].getSoldadoYPos(i);
 	}
+	/**
+	 * Metodo para devolver o tipo do soldado de posicao i na colecao de soldados
+	 * do reino de posicao player desta colecao.
+	 * @param i - posicao do soldado na colecao de soldados do reino.
+	 * @param player - posicao do reino nesta colecao.
+	 * @return - devolve o tipo do soldado.
+	 */
 	public String getReinoSoldadoType(int i, int player) {
 		return r1[player].getSoldadoType(i);
 	}
+	/**
+	 * Metodo para encontra a posicao do soldado na colecao de soldados do reino de posicao player nesta colecao
+	 * atraves das suas coordenadas.
+	 * @param xPos - coordenada x do soldado a encontrar.
+	 * @param yPos - coordenada y do soldado a encontrar.
+	 * @param player - posicao do reino no qual procurar o soldado.
+	 * @return - Devolve -1 se nao houver esse soldado no reino, caso contrario devolve a sua posicao na colecao de
+	 * soldados do reino.
+	 */
 	public int findSoldadoIndex(int xPos, int yPos, int player) {
 		return r1[player].findSoldadoIndex(xPos, yPos);
 	}
+	/**
+	 * Metodo para mover o soldado de posicao i na colecao de soldados, do reino de
+	 * posicao player nesta colecao, para certa direcao.
+	 * @param i - posicao do soldado na colecao de soldados.
+	 * @param direcao - direcao para o qual o soldado deve mover-se.
+	 * @param player - posicao do reino desta colecao.
+	 */
 	public void moveReinoSoldado(int i, String direcao, int player) {
 		r1[player].moveSoldado(i, direcao);
 	}
+	/**
+	 * Metodo para averiguar se houve colisao entre um soldado do reino de posicao player nesta colecao,
+	 * e os outros soldados desse reino.
+	 * @param xPos - coordenada x do soldado.
+	 * @param yPos - coordenada y do soldado.
+	 * @param soldado - posicao do soldado na colecao de soldados do reino.
+	 * @param direcao - direcao para o qual so soldado pretende mover-se.
+	 * @param player - posicao do reino nesta colecao.
+	 * @return - devolve true se houver colisao.
+	 */
 	public boolean reinoSoldadoColision(int xPos,int yPos, int soldado, String direcao, int player) {
 		return r1[player].soldadoColision(xPos, yPos, soldado, direcao);
 	}
+	/**
+	 * Metodo para verificar se o soldado de posicao xPos yPos do reino de posicao player,
+	 * colide com um soldado dos outros reinos da colecao.
+	 * @param xPos - coordenada x do soldado.
+	 * @param yPos - coordenada y do soldado.
+	 * @param player - posicao do reino, ao qual o soldado pertence, nesta colecao.
+	 * @return - devolve true se houver colisao entre soldados.
+	 */
 	public int enemyColisiom(int xPos, int yPos, int player) {
 		int colision=-1;
 		for(int i=0;i<counterReino;i++) {
@@ -122,7 +292,16 @@ public class ReinoColection {
 		}
 		return colision;
 	}
-	// 0 - morre equipa 2     1 - morre equipa 1
+	/**
+	 * Metodo que em caso de colisao, compara os tipos do dois soldados para saber quem morre.
+	 * @param type - tipo do soldado do reino que esta a jogar.
+	 * @param xPos - coordenada x do soldado da equipa que esta a jogar.
+	 * @param yPos - coordenada y do soldado da equipa que esta a jogar.
+	 * @param player - reino que esta a jogar.
+	 * @return - Devolve -1 caso nao haja colisao, 0 se morre o soldado da equipa fora de turno
+	 * e 1 se morre o soldado da equipa que esta a jogar
+	 */
+	// 0 - morre soldado da equipa fora de turno     1 - morre soldado da equipa que esta a jogar
 	public int compareType(String type, int xPos, int yPos, int player) {
 		int fightType=-1;
 		int teamSol2=enemyColisiom(xPos, yPos, player);
@@ -153,7 +332,14 @@ public class ReinoColection {
 		}
 		return fightType;
 	}
-	
+	/**
+	 * Metodo para matar o soldado que perdeu o combate depedendo da comparacao de tipos.
+	 * @param type - tipo do soldado do reino que esta a jogar.
+	 * @param xPos - coordenada x do soldado da equipa que esta a jogar.
+	 * @param yPos - coordenada y do soldado da equipa que esta a jogar.
+	 * @param player - reino que esta a jogar.
+	 * @param soldado - posicao do soldado na colecao do reino que esta a jogar.
+	 */
 	public void killSoldier(String type, int xPos, int yPos, int player, int soldado) {
 		int teamSol2=enemyColisiom(xPos, yPos, player);
 		int indexSol2=findSoldadoIndex(xPos, yPos, teamSol2);
@@ -163,6 +349,14 @@ public class ReinoColection {
 		if(fightCompare==1) 
 			r1[player].killSoldadoFromReino(soldado);
 	}
+	/**
+	 * Metodo para devolver a mensagem de combate, conforme o resultado da comparacao
+	 * @param type - tipo do soldado do reino que esta a jogar.
+	 * @param xPos - coordenada x do soldado da equipa que esta a jogar.
+	 * @param yPos - coordenada y do soldado da equipa que esta a jogar.
+	 * @param player - reino que esta a jogar.
+	 * @return devolve a mensagem de combate.
+	 */
 	public String getFightMessage(String type, int xPos, int yPos, int player) {
 		String message="";
 		int teamSol2=enemyColisiom(xPos, yPos, player);
@@ -176,6 +370,11 @@ public class ReinoColection {
 		
 		return message;
 	}
+	/**
+	 * Metodo para devolver a posicao do reino de nome reino nesta colecao.
+	 * @param reino - nome do reino.
+	 * @return - devolve a posicao do reino nesta colecao.
+	 */
 	public int getReinoWithNameIndex(String reino) {
 		int player=-1;
 		for(int i=0;i<counterReino;i++) {
@@ -185,16 +384,29 @@ public class ReinoColection {
 		}
 		return player;
 	}
+	/**
+	 * Metodo para remover o castelo de nome name do reino de nome reino.
+	 * @param name - nome do castelo a remover.
+	 * @param reino - nome do reino do qual o castelo deve ser removido.
+	 */
 	public void removeCastleFromReino(String name, String reino) {
 		int player=getReinoWithNameIndex(reino);
 		r1[player].removeCastleFromReino(name);
 	}
+	/**
+	 * Metodo para remover o reino de posicao player da colecao.
+	 * @param player - posicao do reino a remover.
+	 */
 	public void removeReino(int player) {
 		for(int i=player;i<counterReino-1;i++) {
 			r1[i]=r1[i+1];
 		}
 		counterReino--;
 	}
+	/**
+	 * Metodo para verificar se algum reino da colecao está "morto".
+	 * @return - devolve -1 se todos os reinos tiverem vivos, caso contrario devolve a sua posicao nesta colecao.
+	 */
 	public int isReinoDead() {
 		int dead =-1;
 		for(int i=0; i<counterReino && dead ==-1;i++) {
@@ -204,6 +416,10 @@ public class ReinoColection {
 		}
 		return dead;
 	}
+	/**
+	 * Metodo para verificar se so ha 1 reino restante na colecao.
+	 * @return devolve true se so restar 1 reino na colecao
+	 */
 	public boolean lastReino() {
 		return counterReino==1;
 	}
